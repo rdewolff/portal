246f0d8e11b3057e59b2e712fee6632d85bed699
Romain de Wolff <rdewolff@gmail.com>
2014-08-14 15:47:59 +0200

    Home page

--- apps/portal/index.js
+++ apps/portal/index.js
@@ -10,6 +10,15 @@ app.serverUse(module, 'derby-stylus');
 app.loadViews(__dirname + '/views');
 app.loadStyles(__dirname + '/styles');
 
-app.get('/', function(page){
-  page.render('home');
+app.get('/', function(page, model){
+
+  var collection = model.query('collection', {});
+
+  model.subscribe(collection, function(err, next) {
+
+    model.ref('_page.collection', collection);
+
+    page.render('home');
+
+  });
 });
